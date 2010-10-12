02b1ed076a9ef79b12ba55fb009bebffe77bebdd
tlh <thunkout@gmail.com>
2010-10-12 03:46:29 -0500

    Combined pickel-create-objects code into one function.

--- pickel.el
+++ pickel.el
@@ -224,38 +224,29 @@ GHIJKLMNOPQRSTUVWXYZ~!@$%^&*|<>"))
 
 ;;; create objects
 
-(defun pickel-create-symbol (sym)
-  "Create SYM."
-  (princ (cond ((eq sym nil) "nil")
-               ((eq sym t) "t")
-               ((intern-soft sym) (format "'%s" sym))
-               (t (format "(m \"%s\")" sym)))))
-
-(defun pickel-create-hash-table (table)
-  "Create TABLE."
-  (princ (format "(h '%s %s %s %s %s)"
-                 (hash-table-test             table)
-                 (hash-table-size             table)
-                 (hash-table-rehash-size      table)
-                 (hash-table-rehash-threshold table)
-                 (hash-table-weakness         table))))
-
 (defun pickel-create-objects ()
   "Print the id's and creators of all objects in BINDINGS."
-  (let ((first t))
-    (pickel-wrap "(" ")"
-      (pickel-dohash (obj id bindings)
-        (if first (setq first nil) (princ " "))
-        (princ id)
-        (princ " ")
+  (pickel-wrap "(" ")"
+    (pickel-dohash (obj id bindings)
+      (princ
+       (format
+        "%s %s " id
         (etypecase obj
-          (integer    (princ obj))
-          (float      (princ obj))
-          (string     (prin1 obj))
-          (cons       (princ "(c)"))
-          (vector     (princ (format "(v %s)" (length obj))))
-          (symbol     (pickel-create-symbol obj))
-          (hash-table (pickel-create-hash-table obj)))))))
+          (integer    obj)
+          (float      obj)
+          (string     (format "%S" obj))
+          (cons       "(c)")
+          (vector     (format "(v %s)" (length obj)))
+          (symbol     (cond ((eq obj nil) "nil")
+                            ((eq obj t)   "t")
+                            ((intern-soft obj) (format "'%s" obj))
+                            (t (format "(m %S)" (symbol-name obj)))))
+          (hash-table (format "(h '%s %s %s %s %s)"
+                              (hash-table-test             obj)
+                              (hash-table-size             obj)
+                              (hash-table-rehash-size      obj)
+                              (hash-table-rehash-threshold obj)
+                              (hash-table-weakness         obj)))))))))
 
 
 ;;; set objects
